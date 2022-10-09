acba18289761cffb13b572f463467757472f20e1
pauljacob <paul.em.jacob@gmail.com>
2022-10-09 02:27:32 -0700

    Initial commit

---
+++ notebooks/in_vehicle_coupon_prediction.py
@@ -0,0 +1,224 @@
+
+
+import pandas as pd
+from sklearn.model_selection import TimeSeriesSplit
+
+
+def initialize_custom_notebook_settings():
+    '''
+    initialize the jupyter notebook display width'''
+        
+    from IPython.core.display import display, HTML
+    display(HTML("<style>.container { width:95% !important; }</style>"))
+
+    
+    pd.options.display.max_columns = 999
+    pd.options.display.max_rows = 999
+
+    pd.set_option('display.max_colwidth', None)
+    pd.options.display.max_info_columns = 999
+
+    
+
+
+
+'''
+Convenience functions: read, sort, print, and save data frame or dictionary.
+'''
+def p(df):
+    '''
+    Return the first 5 and last 5 rows of this DataFrame.'''
+    if df.shape[0] > 6:
+        print(df.shape)
+        return pd.concat([df.head(), df.tail()])
+    else:
+        return df
+
+def rcp(filename, parse_dates=None, index_col=None):
+    '''
+    Read a file from the processed_data folder.'''
+    if parse_dates == None:
+        return pd.read_csv(os.path.join('..', 'data', 'processed', filename), index_col=index_col)
+    else:
+        return pd.read_csv(os.path.join('..', 'data', 'processed', filename), parse_dates=parse_dates,  index_col=index_col)
+
+def rpp(filename, parse_dates=None):
+    '''
+    Save collection and return it.'''
+
+    relative_file_path = os.path.join('..', 'data', 'processed', filename)
+        
+    with (open(relative_file_path, "rb")) as open_file:
+        return pickle.load(open_file)
+    
+def rcr(filename, parse_dates=None):
+    '''
+    Read a file from the raw_data folder.'''
+    if parse_dates == None:
+        return pd.read_csv(os.path.join('..', 'data', 'raw', filename))
+    else:
+        return pd.read_csv(os.path.join('..', 'data', 'raw',  filename), parse_dates=parse_dates)
+
+def sr(df, column_name_list):
+    '''
+    Sort DataFrame by column(s) and reset its index.'''
+    df = df.sort_values(column_name_list)
+    return df.reset_index(drop=True)
+
+def pl(list_):
+    '''
+    Print the list length and return the list.'''
+    print(len(list_))
+    return list_
+
+def pdc(dict_):
+    '''
+    Print the dictionary length and return the dictionary.'''
+    print(len(dict_))
+    return dict_
+
+def save_and_return_data_frame(df, filename, index=False, parse_dates=False, index_label=None):
+    '''
+    Save data frame and return it.'''
+    
+    relative_directory_path = os.path.join('..', 'data', 'processed')
+
+    if not os.path.exists(relative_directory_path):
+        os.mkdir(relative_directory_path)
+
+    relative_file_path = os.path.join(relative_directory_path, filename)
+    if not os.path.exists(relative_file_path):
+        df.to_csv(relative_file_path, index=index, index_label=index_label)
+    elif os.path.exists(relative_file_path):
+        print('This file already exists.')
+        
+    return rcp(filename, parse_dates, index_col=index_label)
+
+
+def save_and_return_collection(data_frame_collection,
+                               filename,
+                               index=False,
+                               parse_dates=None):
+    '''
+    Save collection and return it.'''
+
+    relative_directory_path = os.path.join('..', 'data', 'processed')
+
+    if not os.path.exists(relative_directory_path):
+        os.mkdir(relative_directory_path)
+
+    relative_file_path = os.path.join(relative_directory_path, filename)
+    
+    if not os.path.exists(relative_file_path):
+        file_object_wb =  open(relative_file_path, "wb")
+        pickle.dump(data_frame_collection, file_object_wb)
+        file_object_wb.close()
+
+    elif os.path.exists(relative_file_path):
+        print('This file already exists.')
+        
+    with (open(relative_file_path, "rb")) as open_file:
+        data_frame_collection_readback = pickle.load(open_file)
+
+    return data_frame_collection_readback
+
+
+def return_processed_data_file_if_it_exists(filename,
+                                            parse_dates=False):
+    
+    relative_directory_path = os.path.join('..', 'data', 'processed')
+
+    if not os.path.exists(relative_directory_path):
+        os.mkdir(relative_directory_path)
+
+    relative_file_path = os.path.join(relative_directory_path, filename)
+
+    if os.path.exists(relative_file_path):
+        print('This file already exists')
+        return rcp(filename, parse_dates)
+    else:
+        return pd.DataFrame({})
+
+    
+def return_processed_collection_if_it_exists(filename,
+                                             parse_dates=False):
+    import pickle
+    
+    relative_directory_path = os.path.join('..', 'data', 'processed')
+
+    if not os.path.exists(relative_directory_path):
+        os.mkdir(relative_directory_path)
+
+    relative_file_path = os.path.join(relative_directory_path, filename)
+
+    if os.path.exists(relative_file_path):
+        print('This file already exists')
+        
+        with (open(relative_file_path, "rb")) as openfile:
+            data_frame_collection_readback = pickle.load(openfile)
+        
+        return data_frame_collection_readback
+    
+    else:
+        return None
+    
+    
+def return_figure_if_it_exists(filename):
+
+    import glob
+    import imageio
+
+    image = None
+    for image_path in glob.glob(filename):
+        image = imageio.imread(image_path)
+        
+    return image
+
+    
+
+def show_data_frames_in_memory(dir_):
+    alldfs = [var for var in dir_ if isinstance(eval(var), pd.core.frame.DataFrame)]
+
+    print(alldfs)
+
+
+    
+def get_column_name_list_left_not_in_right(df_left,
+                                           df_right):
+
+    column_name_list_in_both = list(set(df_left.columns).intersection(set(df_right.columns)))
+
+    return [k for k in df_left.columns if k not in column_name_list_in_both]
+##################################################################################################################################
+    
+
+    
+    
+    
+def column_name_value_sets_equal(df, column_name1, column_name2):
+    """
+    Parameters
+    ----------
+    df : DataFrame
+        data frame selecting columns from
+    column_name1 : str
+        first column name to take unique values of
+    column_name2 : str
+        second column name to take unique values of
+        
+    Returns
+    -------
+    returns 1 for column value uniques are equal, otherwise 0
+    """
+    
+    column_name_value_set1 = set(df.loc[:, column_name1].unique())
+    
+    column_name_value_set2 = set(df.loc[:, column_name2].unique())
+    
+    if column_name_value_set1 == column_name_value_set2:
+        return 1
+    elif column_name_value_set1 != column_name_value_set2:
+        return 0
+    
+    
+    
