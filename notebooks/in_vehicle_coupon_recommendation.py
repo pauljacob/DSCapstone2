2bda41f07c2440c18b97a37bcaca0ded3412f07a
pauljacob <paul.em.jacob@gmail.com>
2022-10-26 02:04:48 -0700

    update

--- notebooks/in_vehicle_coupon_recommendation.py
+++ notebooks/in_vehicle_coupon_recommendation.py
@@ -1,11 +1,16 @@
 import os
 import pandas as pd
-import shutil
 
+
+#data wrangling
+from functools import reduce
 
 #ML
 import pickle
 
+#save object
+import zipfile
+import shutil
 
 
 def initialize_custom_notebook_settings():
@@ -27,7 +32,7 @@ def initialize_custom_notebook_settings():
 
 
 '''
-Convenience functions: read, sort, print, and save data frame or dictionary.
+Convenience functions: read, sort, print, and save data frame, dictionary, or model.
 '''
 def p(df):
     '''
@@ -81,6 +86,25 @@ def pdc(dict_):
     print(len(dict_))
     return dict_
 
+
+def show_data_frames_in_memory(dir_):
+    alldfs = [var for var in dir_ if isinstance(eval(var), pd.core.frame.DataFrame)]
+
+    print(alldfs)
+
+
+    
+def get_column_name_list_left_not_in_right(df_left,
+                                           df_right):
+
+    column_name_list_in_both = list(set(df_left.columns).intersection(set(df_right.columns)))
+
+    return [k for k in df_left.columns if k not in column_name_list_in_both]
+
+
+##################################################################################################################################
+#save and return object
+##################################################################################################################################
 def save_and_return_data_frame(df, filename, index=False, parse_dates=False, index_label=None):
     '''
     Save data frame and return it.'''
@@ -99,10 +123,7 @@ def save_and_return_data_frame(df, filename, index=False, parse_dates=False, ind
     return rcp(filename, parse_dates, index_col=index_label)
 
 
-def save_and_return_collection(data_frame_collection,
-                               filename,
-                               index=False,
-                               parse_dates=None):
+def save_and_return_collection(data_frame_collection, filename):
     '''
     Save collection and return it.'''
 
@@ -127,8 +148,79 @@ def save_and_return_collection(data_frame_collection,
     return data_frame_collection_readback
 
 
-def return_processed_data_file_if_it_exists(filename,
-                                            parse_dates=False):
+def save_and_return_model(model, filename):
+    
+    relative_directory_path = os.path.join('..', 'models')
+
+    #make relative file direactory path if it doesn't exist
+    if not os.path.exists(relative_directory_path):
+        os.mkdir(relative_directory_path)
+        
+    #get relative file path name
+    relative_file_path = os.path.join(relative_directory_path, filename)
+
+    #if model file already exists, say it
+    if os.path.exists(relative_file_path):
+            print('This file already exists.')
+
+    #if model file doesn't exist, then save it
+    elif not os.path.exists(relative_file_path):
+        file_object_wb =  open(relative_file_path, "wb")
+        pickle.dump(model, file_object_wb)
+        file_object_wb.close()
+    
+    #readback model file
+    with (open(relative_file_path, "rb")) as open_file:
+        model_readback = pickle.load(open_file)
+
+    return model_readback
+
+
+def save_and_return_model2(model, filename, add_compressed_file=False):
+
+    
+    relative_directory_path = os.path.join('..', 'models')
+
+    #make relative file direactory path if it doesn't exist
+    if not os.path.exists(relative_directory_path):
+        os.mkdir(relative_directory_path)
+        
+    #get relative file path name
+    relative_file_path = os.path.join(relative_directory_path, filename)
+#     relative_file_path_zipped = os.path.join(relative_directory_path, filename) + '.zip'
+
+    
+#     #if zipped model file already exists, say it
+#     if os.path.exists(relative_file_path_zipped):
+#             print('This zipped file already exists.')
+
+#     #if zipped model file doesn't exist, then save it (zipped)
+#     elif not os.path.exists(relative_file_path_zipped) and add_compressed_file == True:
+#         shutil.make_archive(relative_file_path, 'zip', relative_directory_path)
+        
+
+        
+    if os.path.exists(relative_file_path):
+            print('This file already exists.')
+            
+    #if model file doesn't exist, then save it
+    elif not os.path.exists(relative_file_path):
+        file_object_wb =  open(relative_file_path, "wb")
+        pickle.dump(model, file_object_wb)
+        file_object_wb.close()
+        
+    
+    #readback model file
+    with (open(relative_file_path, "rb")) as open_file:
+        model_readback = pickle.load(open_file)
+
+    return model_readback
+
+##################################################################################################################################
+#return object if it exists
+##################################################################################################################################
+
+def return_processed_data_file_if_it_exists(filename, parse_dates=False):
     
     relative_directory_path = os.path.join('..', 'data', 'processed')
 
@@ -144,9 +236,7 @@ def return_processed_data_file_if_it_exists(filename,
         return pd.DataFrame({})
 
     
-def return_processed_collection_if_it_exists(filename,
-                                             parse_dates=False):
-    import pickle
+def return_processed_collection_if_it_exists(filename, parse_dates=False):
     
     relative_directory_path = os.path.join('..', 'data', 'processed')
 
@@ -166,6 +256,25 @@ def return_processed_collection_if_it_exists(filename,
     else:
         return None
     
+def return_saved_model_if_it_exists(filename):
+    relative_directory_path = os.path.join('..', 'models')
+
+    if not os.path.exists(relative_directory_path):
+        os.mkdir(relative_directory_path)
+
+    relative_file_path = os.path.join(relative_directory_path, filename)
+
+    if os.path.exists(relative_file_path):
+        print('This file already exists')
+        
+        with (open(relative_file_path, "rb")) as openfile:
+            model_readback = pickle.load(openfile)
+            
+        return model_readback
+    else:
+        return None
+    
+    
     
 def return_figure_if_it_exists(filename):
 
@@ -178,21 +287,26 @@ def return_figure_if_it_exists(filename):
         
     return image
 
-    
-
-def show_data_frames_in_memory(dir_):
-    alldfs = [var for var in dir_ if isinstance(eval(var), pd.core.frame.DataFrame)]
-
-    print(alldfs)
+################################################################################################################################## 
 
 
-    
-def get_column_name_list_left_not_in_right(df_left,
-                                           df_right):
 
-    column_name_list_in_both = list(set(df_left.columns).intersection(set(df_right.columns)))
 
-    return [k for k in df_left.columns if k not in column_name_list_in_both]
+
+
+##################################################################################################################################
+#data wrangling
+##################################################################################################################################
+
+def merge_data_frame_list(data_frame_list):
+    return reduce(lambda left, right : pd.merge(left,
+                                                right,
+                                                on=[column_name for column_name in left if column_name in right],
+                                                how='inner'), 
+                  data_frame_list)
+
+
+
 ##################################################################################################################################
     
 
@@ -235,97 +349,7 @@ def column_name_value_sets_equal(df, column_name1, column_name2):
 
 
 
-def return_saved_model_if_it_exists(filename):
-    
-    relative_directory_path = os.path.join('..', 'models')
-
-    if not os.path.exists(relative_directory_path):
-        os.mkdir(relative_directory_path)
-
-    relative_file_path = os.path.join(relative_directory_path, filename)
-
-    if os.path.exists(relative_file_path):
-        print('This file already exists')
-        
-        with (open(relative_file_path, "rb")) as openfile:
-            model_readback = pickle.load(openfile)
-        
-        return model_readback
-    
-    else:
-        return None
 
 
-def save_and_return_model(model,
-                          filename):
-    
-    relative_directory_path = os.path.join('..', 'models')
-
-    #make relative file direactory path if it doesn't exist
-    if not os.path.exists(relative_directory_path):
-        os.mkdir(relative_directory_path)
-        
-    #get relative file path name
-    relative_file_path = os.path.join(relative_directory_path, filename)
-
-    #if model file already exists, say it
-    if os.path.exists(relative_file_path):
-            print('This file already exists.')
-
-    #if model file doesn't exist, then save it
-    elif not os.path.exists(relative_file_path):
-        file_object_wb =  open(relative_file_path, "wb")
-        pickle.dump(model, file_object_wb)
-        file_object_wb.close()
-    
-    #readback model file
-    with (open(relative_file_path, "rb")) as open_file:
-        model_readback = pickle.load(open_file)
-
-    return model_readback
-
-
-def save_and_return_model2(model,
-                           filename,
-                           add_compressed_file=False):
-    import zipfile
-    import shutil
-    
-    relative_directory_path = os.path.join('..', 'models')
-
-    #make relative file direactory path if it doesn't exist
-    if not os.path.exists(relative_directory_path):
-        os.mkdir(relative_directory_path)
-        
-    #get relative file path name
-    relative_file_path = os.path.join(relative_directory_path, filename)
-#     relative_file_path_zipped = os.path.join(relative_directory_path, filename) + '.zip'
-
-    
-#     #if zipped model file already exists, say it
-#     if os.path.exists(relative_file_path_zipped):
-#             print('This zipped file already exists.')
-
-#     #if zipped model file doesn't exist, then save it (zipped)
-#     elif not os.path.exists(relative_file_path_zipped) and add_compressed_file == True:
-#         shutil.make_archive(relative_file_path, 'zip', relative_directory_path)
-        
-
-        
-    if os.path.exists(relative_file_path):
-            print('This file already exists.')
-            
-    #if model file doesn't exist, then save it
-    elif not os.path.exists(relative_file_path):
-        file_object_wb =  open(relative_file_path, "wb")
-        pickle.dump(model, file_object_wb)
-        file_object_wb.close()
-        
-    
-    #readback model file
-    with (open(relative_file_path, "rb")) as open_file:
-        model_readback = pickle.load(open_file)
-
-    return model_readback
 
 
